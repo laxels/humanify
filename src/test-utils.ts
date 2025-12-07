004ab52bdaa52e7baae08510832ec4808054b8ca
Axel Setyanto <laxels@gmail.com>
2025-12-06 16:46:54 -0800

    rm unused

--- src/test-utils.ts
+++ src/test-utils.ts
@@ -1,13 +1,5 @@
-import assert from "assert";
 import { spawn } from "child_process";
 import { verbose } from "./verbose.js";
-
-export function assertMatches(actual: string, expected: string[]) {
-  assert(
-    expected.some((str) => actual.toLowerCase().includes(str.toLowerCase())),
-    `Expected ${actual} to be one of ${JSON.stringify(expected)}`
-  );
-}
 
 export async function humanify(...argv: string[]) {
   const extraArgs = argv.includes("local") ? ["--seed", "1"] : [];
@@ -33,14 +25,4 @@ export async function humanify(...argv: string[]) {
   verbose.log("stderr", stderr.join(""));
 
   return { stdout: stdout.join(""), stderr: stderr.join("") };
-}
-
-export function ensure<T>(
-  value: NonNullable<T> | undefined | null,
-  message: string = "Value was null or undeined"
-): NonNullable<T> {
-  if (value === undefined || value === null) {
-    throw new Error(message);
-  }
-  return value;
 }
