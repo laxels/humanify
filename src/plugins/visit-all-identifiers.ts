babe58eb2001364c79acbd06482596adcb0c4440
Axel Setyanto <laxels@gmail.com>
2025-12-07 15:43:43 -0800

    rename

--- src/plugins/visit-all-identifiers.ts
+++ src/plugins/visit-all-identifiers.ts
@@ -30,32 +30,32 @@ export async function visitAllIdentifiers(
     throw new Error("Failed to parse code");
   }
 
-  const scopes = findScopes(ast);
-  const numRenamesExpected = scopes.length;
+  const bindingPaths = findBindingIdentifiers(ast);
+  const numRenamesExpected = bindingPaths.length;
 
-  for (const smallestScope of scopes) {
-    if (hasVisited(smallestScope, visited)) continue;
+  for (const bindingPath of bindingPaths) {
+    if (hasVisited(bindingPath, visited)) continue;
 
-    const smallestScopeNode = smallestScope.node;
-    if (smallestScopeNode.type !== "Identifier") {
+    const identifierNode = bindingPath.node;
+    if (identifierNode.type !== "Identifier") {
       throw new Error("No identifiers found");
     }
 
-    const surroundingCode = scopeToString(smallestScope, contextWindowSize);
-    const renamed = await visitor(smallestScopeNode.name, surroundingCode);
-    if (renamed !== smallestScopeNode.name) {
+    const surroundingCode = scopeToString(bindingPath, contextWindowSize);
+    const renamed = await visitor(identifierNode.name, surroundingCode);
+    if (renamed !== identifierNode.name) {
       let safeRenamed = toIdentifier(renamed);
       while (
         renames.has(safeRenamed) ||
-        smallestScope.scope.hasBinding(safeRenamed)
+        bindingPath.scope.hasBinding(safeRenamed)
       ) {
         safeRenamed = `_${safeRenamed}`;
       }
       renames.add(safeRenamed);
 
-      smallestScope.scope.rename(smallestScopeNode.name, safeRenamed);
+      bindingPath.scope.rename(identifierNode.name, safeRenamed);
     }
-    markVisited(smallestScope, smallestScopeNode.name, visited);
+    markVisited(bindingPath, identifierNode.name, visited);
 
     onProgress?.(visited.size, numRenamesExpected);
   }
@@ -68,20 +68,21 @@ export async function visitAllIdentifiers(
   return stringified.code;
 }
 
-function findScopes(ast: Node): NodePath<Identifier>[] {
-  const scopes: [nodePath: NodePath<Identifier>, scopeSize: number][] = [];
+function findBindingIdentifiers(ast: Node): NodePath<Identifier>[] {
+  const pathsWithScopeSize: [nodePath: NodePath<Identifier>, scopeSize: number][] = [];
   traverse(ast, {
     BindingIdentifier(path: NodePath<Identifier>) {
       const bindingBlock = closestSurroundingContextPath(path).scope.block;
-      const pathSize = bindingBlock.end! - bindingBlock.start!;
+      const scopeSize = bindingBlock.end! - bindingBlock.start!;
 
-      scopes.push([path, pathSize]);
+      pathsWithScopeSize.push([path, scopeSize]);
     },
   });
 
-  scopes.sort((a, b) => b[1] - a[1]);
+  // Sort by scope size descending (largest first) so outer scopes are renamed before inner
+  pathsWithScopeSize.sort((a, b) => b[1] - a[1]);
 
-  return scopes.map(([nodePath]) => nodePath);
+  return pathsWithScopeSize.map(([nodePath]) => nodePath);
 }
 
 function hasVisited(path: NodePath<Identifier>, visited: Set<string>) {
